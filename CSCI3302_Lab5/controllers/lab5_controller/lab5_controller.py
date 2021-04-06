381829f24b23e43fa83f5b34344dc92739f65016
Rkenfield <Ryanjkenfield@aol.com>
2021-04-05 19:53:59 -0600

    lab 5 almost done

--- CSCI3302_Lab5/controllers/lab5_controller/lab5_controller.py
+++ CSCI3302_Lab5/controllers/lab5_controller/lab5_controller.py
@@ -68,8 +68,9 @@ display = robot.getDevice("display")
 #pose_y     = 8.9
 #pose_theta = 0
 
-pose_x     = 4.47
-pose_y     = 8.06
+#based on the given maps in the lab description
+pose_x     = 4.49
+pose_y     = 4.8
 pose_theta = 0
 
 #pose_x = 4.266
@@ -83,14 +84,14 @@ vR = 0
 #variable initialization for controller
 Dist_Error = 0
 Bearing_Error = 0
-gain = .25
+gain = .2
 xR = 0
 thetaR = 0
 final = 0
 
 ##################### IMPORTANT #####################
 # Set the mode here. Please change to 'autonomous' before submission
-# mode = 'manual' # Part 1.1: manual mode
+#mode = 'manual' # Part 1.1: manual mode
 mode = 'planner'
 #mode = 'autonomous'
 
@@ -116,18 +117,19 @@ if mode == 'planner':
     #start_w = (math.cos(pose_theta)*rx - math.sin(pose_theta)*ry + pose_x,-(math.sin(pose_theta)*rx + math.cos(pose_theta)*ry) + pose_y)
     
     end_w = (10,7) # (Pose_X, Pose_Z) in meters
-    
+    #end_w = (7.6,2.5) 
     
     
     
     
     # Convert the start_w and end_W from webot's coordinate frame to map's
-    #start = (int(start_w[0]*30),(360-int(start_w[1]*30))) # (x, y) in 360x360 map
-    #end = (int(end_w[0]*30),(360-int(end_w[1]*30))) # (x, y) in 360x360 map
+    start = (int(start_w[0]*30)-1,(int(start_w[1]*30)-1)) # (x, y) in 360x360 map
+    end = (int(end_w[0]*30)-1,(int(end_w[1]*30)-1)) # (x, y) in 360x360 map
     
     #start =((360-int(start_w[1]*30)-1),int(start_w[0]*30)-1)
     #end = ((360-int(end_w[1]*30)-1),int(end_w[0]*30)-1)
-    
+    #print(start)
+    #print(end)
     
     
     #display.setColor( int(0xFFFFFF))
@@ -152,113 +154,116 @@ if mode == 'planner':
         
 # Part 2.3: Implement A* or Dijkstra's
     def path_planner(map, start, end):
-    
-          
-        print(map[start[0],start[1]])    
+        # print(map[start[1],start[0]])
         #:param map: A 2D numpy array of size 360x360 representing the world's cspace with 0 as free space and 1 as obstacle
         #:param start: A tuple of indices representing the start cell in the map
         #:param end: A tuple of indices representing the end cell in the map
         #:return: A list of tuples as a path from the given start to the given end in the given maze
+        if map[start[0]][start[1]] == 1 or map[end[0]][end[1]] == 1:
+            #print("Start or Goal on obstacle")
+            return []
         
-        #map[start[0],start[1]] = 0
-     
-         
+        # map[start[0],start[1]] = 0
         startN = pathNode(None, start)
-        endN = pathNode(None, end)   
-
+        endN = pathNode(None, end)
+        
         openL = []
         closedL = []
-    
+        
         openL.append(startN)
-        #count = 0
-        #times = 0
-        while(len(openL) > 0):
+        # count = 0
+        # times = 0
+        while (len(openL) > 0):
+            #print(len(openL))
             cIndex = 0
             cNode = openL[cIndex]
         
             for i in range(len(openL)):
-                if(openL[i].f < cNode.f):
+                if (openL[i].f < cNode.f):
                     cIndex = i
                     cNode = openL[i]
-                    
-            #print(cNode.f)    
+        
+            # print(cNode.f)
             closedL.append(cNode)
+            #print(openL[cIndex].position)
             openL.pop(cIndex)
         
-            if(cNode == endN):
+            if cNode == endN:
+                #print("Goal found")
                 path = []
                 current = cNode
-                
-                #when end node is found work back to the start node and save the path
-                while(current is not None):
+        
+                # when end node is found work back to the start node and save the path
+                while current is not None:
                     path.append(current.position)
                     current = current.parent
-                #path is reversed since it works backwards so flip when returning    
-                return(np.flip(path))
-           
+                # path is reversed since it works backwards so flip when returning
+                return list(reversed(path))
         
-           
-            #get surrounding indeces from map
-            adjacent = [] 
-            
-            for i in [(-1,0),(1,0),(0,-1),(0,1),(-1,-1),(1,1),(-1,1),(1,-1)]:
-                
+            # get surrounding indeces from map
+            adjacent = []
+            for i in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1)]:
+        
                 nPos = (cNode.position[0] + i[0], cNode.position[1] + i[1])
-                #make sure indeces are inside the bounds of 360
-                if(nPos[0] < 360 and nPos[0] >= 0 and nPos[1] < 360 and nPos[1] >= 0):
-                        #check if path is open
-                    if(map[nPos[0]][nPos[1]] != 1):
+                # make sure indeces are inside the bounds of 360
+                if (nPos[0] < 360 and nPos[0] >= 0 and nPos[1] < 360 and nPos[1] >= 0):
+                    # check if path is open
+                    if (map[nPos[0]][nPos[1]] == 0):
                         adjacentNode = pathNode(cNode, nPos)
                         adjacent.append(adjacentNode)
                     else:
-                        adjacentNode = pathNode(cNode, nPos)
-                        closedL.append(adjacentNode)
-                         
-                
-             
+                        continue
+        
             # check the possible next positions
             for neighbor in adjacent:
+        
+                # make sure the node hasnt been visited
+                if neighbor in closedL:  # SHIV : This is O(n) vs your O(n^2) implementation
+                    continue
                 
-                #make sure the node hasnt been visited
-                for i in closedL:
-                    if neighbor == i:
-                        continue
-                
-                #otherwise set the f value for the node/path 
-                
-                #Add root 2 to distance from source for diagonal children since moving diagonal is root 2 not 1
-                if(neighbor.position[0] != neighbor.parent.position[0] and neighbor.position[1] != neighbor.parent.position[1]):
-                    neighbor.d = cNode.d + np.sqrt(2) 
+        
+                # otherwise set the f value for the node/path
+        
+                # Add root 2 to distance from source for diagonal children since moving diagonal is root 2 not 1
+                if (neighbor.position[0] != neighbor.parent.position[0] and neighbor.position[1] != neighbor.parent.position[1]):
+                    neighbor.d = cNode.d + np.sqrt(2)
                 else:
                     neighbor.d = cNode.d + 1
-                    
-                    
-                    
-                neighbor.h = ((endN.position[0] - neighbor.position[0])**2) + ((endN.position[1] - neighbor.position[1]) ** 2)
-                #neighbor.h = np.linalg.norm(np.array(neighbor.position) - np.array(endN.position))
+        
+                # SHIV: The heuristic was not admissble unless you do the square root
+                neighbor.h = math.sqrt(
+                    ((endN.position[0] - neighbor.position[0]) ** 2) + ((endN.position[1] - neighbor.position[1]) ** 2))
+                # neighbor.h = np.linalg.norm(np.array(neighbor.position) - np.array(endN.position))
                 neighbor.f = neighbor.d + neighbor.h
-                
-                #double check that current node has lowest f value ensuring the minimum path is taken
+        
+                # double check that current node has lowest f value ensuring the minimum path is taken
                 for i in openL:
-                    if neighbor == i and neighbor.d > i.d:
-                        continue
-                        
-                openL.append(neighbor)
-                
-        return([])        
-         
+                    if neighbor == i:
+                        if neighbor.d > i.d:
+                             break
+                        else:
+                            # SHIV: TODO: Update the d score of the node in open list along with it's f score and parent
+                            i.d = neighbor.d
+                            i.f = i.d + i.h
+                            i.parent = neighbor.parent
+                            break
+                else:
+                    openL.append(neighbor)
+        
+        return []
+    
+             
 # Part 2.1: Load map (map.npy) from disk and visualize it
     map = np.load("map.npy")
-    nmap = np.rot90(map,1,(1,0))
+    
  
-    map[map>.5] = 1
-    map[map<=.5] = 0
-    #plt.imshow(nmap)
-    #plt.show()
+    map[map>.7] = 1
+    map[map<=.7] = 0
+    
  
 # Part 2.2: Compute an approximation of the "configuration space"
     
-    filter = np.ones((7,7))
+    filter = np.ones((16,16))
     cmap = convolve2d(map,filter)
     cmap[cmap>=1] = 1
     plt.imshow(cmap)
@@ -266,28 +271,20 @@ if mode == 'planner':
 
 # Part 2.3 continuation: Call path_planner
     path = path_planner(cmap,start,end)
-    print(len(path))
+    #print(len(path))
 # Part 2.4: Turn paths into goal points and save on disk as path.npy and visualize it
     
     goalPoints = []
     for i in range(len(path)):
-        x = path[i][0]
-        y = path[i][1]
+        x = float(path[i][0])
+        y = float(path[i][1])
         #print(x)
         #print(y)
-        
-        goalPoint = ((y + 1)/30,((-x + 359)/30))
-        
+        goalPoint = (x/30,y/30)
         goalPoints.append(goalPoint)
-        
         final = len(goalPoints)
-    
-        
-        
-        
-        
-        
-    np.save("path.npy",path)
+
+    np.save("path.npy", path)
     np.save("goalPath.npy",goalPoints)
 
 
@@ -303,17 +300,11 @@ if mode == 'autonomous':
 # Part 3.1: Load path from disk and visualize it (Make sure its properly indented)
     path = np.load("goalPath.npy")
     
-    
     for i in range(len(path)):
-        x = path[i][0]
-        y = path[i][1]
-        print((x,y))
-        
         display.setColor(int(0xFF0000))
-        display.drawPixel(int(x),int(y))
-        
-
-state = 1 # use this to iterate through your path
+        display.drawPixel(360-int(path[i][1]*30),int(path[i][0]*30))
+ 
+    state = 0 # use this to iterate through your path
 
 while robot.step(timestep) != -1 and mode != 'planner':
 
@@ -353,33 +344,35 @@ while robot.step(timestep) != -1 and mode != 'planner':
             # You will eventually REPLACE the following 2 lines with a more robust version of map
             # and gray drawing that has more levels than just 0 and 1.
             
-            display.setColor(0xFFFFFF)
-            display.drawPixel(360-int(wy*30),int(wx*30))
+            #display.setColor(0xFFFFFF)
+            #display.drawPixel(360-int(wy*30),int(wx*30))
             
-            if(360-int(wy*30)-1 < 360 and int(wx*30)-1 < 360):
-            
-                if(map[360-int(wy*30)-1][int(wx*30)-1] + .01 <= 1):
-            
-                    map[360-int(wy*30)-1][int(wx*30)-1] = map[360-int(wy*30)-1][int(wx*30)-1] + .01  
-                 
-                else:
-            
-                    map[360-int(wy*30)-1][int(wx*30)-1] = 1
-            
-                if(map[360-int(wy*30)-1][int(wx*30)-1] >.5):
+            if(int(wx*30)-1 < 360 and int(wy*30)-1 < 360):
                 
-                    map[360-int(wy*30)-1][int(wx*30)-1] = 1
-                    display.setColor( int((((map[360-int(wy*30)-1][int(wx*30)-1]*256)**2) + (map[360-int(wy*30)-1][int(wx*30)-1]*256) +  map[360-int(wy*30)-1][int(wx*30)-1])*255))
+            
+                if(map[int(wx*30)-1][int(wy*30)-1] < 1):
+            
+                    map[int(wx*30)-1][int(wy*30)-1] =  (map[int(wx*30)-1][int(wy*30)-1] + 0.01)
+                      
+                elif (map[int(wx*30)-1][int(wy*30)-1] > 1):
+                    
+                    map[int(wx*30)-1][int(wy*30)-1] = 1
+            
+                if(map[int(wx*30)-1][int(wy*30)-1] >.7):
+                   
+                    
+                    map[int(wx*30)-1][int(wy*30)-1] = 1
+                    
+                    display.setColor( int((((map[int(wx*30)-1][int(wy*30)-1]*256)**2) + (map[int(wx*30)-1][int(wy*30)-1]*256) + map[int(wx*30)-1][int(wy*30)-1])*255))
+                   
                     display.drawPixel(360-int(wy*30),int(wx*30))
                     
-                else:
-                
-                    map[360-int(wy*30)-1][int(wx*30)-1] = 0
+              
                     
-    display.setColor(int(0xFF0000))
-    display.drawPixel(360-int(pose_y*30),int(pose_x*30))
-
-
+            display.setColor(int(0xFF0000))
+            display.drawPixel(360-int(pose_y*30),int(pose_x*30))
+            
+    
 
 ###################
 #
@@ -406,7 +399,7 @@ while robot.step(timestep) != -1 and mode != 'planner':
             vR = 0
         elif key == ord('S'):
 # Part 1.4: Save map to disc
-            np.save('map.npy',map)
+            np.save('map.npy', map)
             print("Map file saved")
         elif key == ord('L'):
             # You will not use this portion but here's an example for loading saved a numpy array
@@ -416,71 +409,74 @@ while robot.step(timestep) != -1 and mode != 'planner':
             vL *= 0.75
             vR *= 0.75
     else: # not manual mode
-        pass
-# Part 3.2: Feedback controller
-    if(state < len(path)):
-        #STEP 1: Calculate the error    
-    
-        Dist_Error = math.sqrt(((pose_x - path[state][0])**2) + ((pose_y - (path[state][1]))**2))
-   
-        Bearing_Error = math.atan2((path[state][1] - pose_y) , (path[state][0] - pose_x)) + pose_theta
-    
-
-        #STEP 2: Controller
-        xR = Dist_Error 
-        thetaR = Bearing_Error 
        
-        #print(Bearing_Error)
+# Part 3.2: Feedback controller
+        if(state < len(path)):
+            #STEP 1: Calculate the error    
         
-        #STEP 3: Compute wheelspeeds
-        if(Dist_Error > .3):
+            Dist_Error = math.sqrt(((pose_x - path[state][0])**2) + ((pose_y - (path[state][1]))**2))
+       
+            Bearing_Error = math.atan2((path[state][1] - pose_y) , (path[state][0] - pose_x)) + pose_theta
+        
     
-            if(Bearing_Error < -.2):
-                
+            #STEP 2: Controller
+            xR = Dist_Error 
+            thetaR = Bearing_Error 
+           
+            #print(Bearing_Error)
             
-                if(abs(thetaR * MAX_SPEED * xR) > MAX_SPEED):
-                    vR = MAX_SPEED
-                else:
-                    vR = abs(thetaR * MAX_SPEED * xR)
-                vL = -vR
-                
-                  
-            elif(Bearing_Error > .2):
-                
+            #STEP 3: Compute wheelspeeds
+            if(Dist_Error > gain):
         
-                if(abs(thetaR * MAX_SPEED * xR) > MAX_SPEED):
-                    vL = MAX_SPEED
-                else:
-                    vL = abs(thetaR * MAX_SPEED * xR)
+                if(Bearing_Error < -gain):
+                    
                 
-                vR = -vL
+                    if(abs(thetaR * MAX_SPEED * xR) > MAX_SPEED):
+                        vR = MAX_SPEED
+                    else:
+                        vR = abs(thetaR * MAX_SPEED * xR)
+                    vL = -vR
+                      
+                elif(Bearing_Error > gain):
+                    
             
+                    if(abs(thetaR * MAX_SPEED * xR) > MAX_SPEED):
+                        vL = MAX_SPEED
+                    else:
+                        vL = abs(thetaR * MAX_SPEED * xR)
+                    
+                    vR = -vL
+                
+                
+                else:    
+                    if(xR*MAX_SPEED > MAX_SPEED):
+                        vL = MAX_SPEED
+                        vR = MAX_SPEED
+                    else:
+                        vL = xR*MAX_SPEED
+                        vR = xR*MAX_SPEED
+            else:
+                vL = 0
+                vR = 0
+        
+                state+=1
             
-            else:    
-                if(xR*MAX_SPEED > MAX_SPEED):
-                    vL = MAX_SPEED
-                    vR = MAX_SPEED
-                else:
-                    vL = xR*MAX_SPEED
-                    vR = xR*MAX_SPEED
         else:
-            vL = 0
-            vR = 0
+           vL = 0
+           vR = 0  
     
-            state+=1
-        
-    else:
-       vL = 0
-       vR = 0  
-
-    # Normalize wheelspeed
-    # Keep the max speed a bit less to minimize the jerk in motion
-    if(vL >= MAX_SPEED):
-        vL = MAX_SPEED
-        vR = vR
-    if(vR >= MAX_SPEED):
-        vR = MAX_SPEED
-        vL = vL
+        # Normalize wheelspeed
+        # Keep the max speed a bit less to minimize the jerk in motion
+        if(vL >= MAX_SPEED):
+            vL = MAX_SPEED/2
+            vR = vR/2
+        if(vR >= MAX_SPEED):
+            vR = MAX_SPEED/2
+            vL = vL/2
+            
+        elif( vL == vR):
+            vL *= 0.75
+            vR *= 0.75
     
 
 
